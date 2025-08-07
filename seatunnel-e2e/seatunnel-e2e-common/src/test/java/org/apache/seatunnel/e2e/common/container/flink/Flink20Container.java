0fe41985451fb7fdcac9596a181db99c54963638
zengyi <zengyi@chinatelecom.cn>
2025-08-07 18:23:35 +0800

    [improve][start] test Flink20Container

--- seatunnel-e2e/seatunnel-e2e-common/src/test/java/org/apache/seatunnel/e2e/common/container/flink/Flink20Container.java
+++ seatunnel-e2e/seatunnel-e2e-common/src/test/java/org/apache/seatunnel/e2e/common/container/flink/Flink20Container.java
@@ -73,20 +73,11 @@ public class Flink20Container extends AbstractTestFlinkContainer {
 
     @Override
     protected List<String> getFlinkProperties() {
-        // Flink 1.20 uses new YAML parser that requires valid YAML document format
-        // We need to return a single YAML document string instead of individual properties
+        // Flink 1.20 Docker image may still expect key-value format for FLINK_PROPERTIES
         return Arrays.asList(
-                "---",
-                "# Flink 1.20 Configuration",
-                "jobmanager:",
-                "  rpc:",
-                "    address: jobmanager",
-                "taskmanager:",
-                "  numberOfTaskSlots: 10",
-                "parallelism:",
-                "  default: 4",
-                "env:",
-                "  java:",
-                "    opts: \"-Doracle.jdbc.timezoneAsRegion=false\"");
+                "jobmanager.rpc.address: jobmanager",
+                "taskmanager.numberOfTaskSlots: 10",
+                "parallelism.default: 4",
+                "env.java.opts: \"-Doracle.jdbc.timezoneAsRegion=false\"");
     }
 }
