e16a8fd9329e9611a0b5fda9b44557983f0ae067
zengyi <zengyi@chinatelecom.cn>
2025-08-13 10:02:31 +0800

    [improve][start] revert 0811

--- seatunnel-translation/seatunnel-translation-flink/seatunnel-translation-flink-20/src/main/java/org/apache/seatunnel/translation/flink/sink/FlinkSinkWriter.java
+++ seatunnel-translation/seatunnel-translation-flink/seatunnel-translation-flink-20/src/main/java/org/apache/seatunnel/translation/flink/sink/FlinkSinkWriter.java
@@ -18,133 +18,65 @@
 package org.apache.seatunnel.translation.flink.sink;
 
 import org.apache.seatunnel.api.common.metrics.Counter;
-import org.apache.seatunnel.api.common.metrics.Meter;
 import org.apache.seatunnel.api.common.metrics.MetricNames;
 import org.apache.seatunnel.api.common.metrics.MetricsContext;
-import org.apache.seatunnel.api.sink.MultiTableResourceManager;
-import org.apache.seatunnel.api.sink.SupportResourceShare;
-import org.apache.seatunnel.api.sink.event.WriterCloseEvent;
+import org.apache.seatunnel.api.sink.SinkWriter;
 import org.apache.seatunnel.api.table.type.SeaTunnelRow;
 
-import org.apache.flink.api.connector.sink2.CommittingSinkWriter;
-import org.apache.flink.api.connector.sink2.StatefulSinkWriter;
+import org.apache.flink.api.connector.sink2.Sink;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.io.InvalidClassException;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-/**
- * The sink writer implementation for Flink 1.20+ using sink2 API, adapted from common module with
- * full lifecycle management, state support and committer support.
- *
- * @param <InputT> The generic type of input data
- * @param <CommT> The generic type of commit message
- * @param <WriterStateT> The generic type of writer state
- */
 @Slf4j
-public class FlinkSinkWriter<InputT, CommT, WriterStateT>
-        implements CommittingSinkWriter<InputT, CommitWrapper<CommT>>,
-                StatefulSinkWriter<InputT, FlinkWriterState<WriterStateT>> {
+public class FlinkSinkWriter
+        implements org.apache.flink.api.connector.sink2.SinkWriter<SeaTunnelRow> {
 
-    private final org.apache.seatunnel.api.sink.SinkWriter<SeaTunnelRow, CommT, WriterStateT>
-            sinkWriter;
-    private final org.apache.seatunnel.api.sink.SinkWriter.Context context;
+    private final SinkWriter<SeaTunnelRow, ?, ?> sinkWriter;
     private final Counter sinkWriteCount;
     private final Counter sinkWriteBytes;
-    private final Meter sinkWriterQPS;
-    private long checkpointId;
-    private MultiTableResourceManager resourceManager;
+    private volatile boolean closed = false;
 
-    FlinkSinkWriter(
-            org.apache.seatunnel.api.sink.SinkWriter<SeaTunnelRow, CommT, WriterStateT> sinkWriter,
-            long checkpointId,
-            org.apache.seatunnel.api.sink.SinkWriter.Context context) {
-        this.context = context;
+    public FlinkSinkWriter(
+            SinkWriter<SeaTunnelRow, ?, ?> sinkWriter,
+            Sink.InitContext context,
+            MetricsContext metricsContext) {
         this.sinkWriter = sinkWriter;
-        this.checkpointId = checkpointId;
-        MetricsContext metricsContext = context.getMetricsContext();
         this.sinkWriteCount = metricsContext.counter(MetricNames.SINK_WRITE_COUNT);
         this.sinkWriteBytes = metricsContext.counter(MetricNames.SINK_WRITE_BYTES);
-        this.sinkWriterQPS = metricsContext.meter(MetricNames.SINK_WRITE_QPS);
-
-        if (sinkWriter instanceof SupportResourceShare) {
-            resourceManager =
-                    ((SupportResourceShare) sinkWriter).initMultiTableResourceManager(1, 1);
-            ((SupportResourceShare) sinkWriter).setMultiTableResourceManager(resourceManager, 0);
-        }
-
-        log.info("FlinkSinkWriter initialized for Flink 1.20+ with checkpointId: {}", checkpointId);
     }
 
     @Override
     public void write(
-            InputT element, org.apache.flink.api.connector.sink2.SinkWriter.Context context)
+            SeaTunnelRow element, org.apache.flink.api.connector.sink2.SinkWriter.Context context)
             throws IOException, InterruptedException {
         if (element == null) {
             return;
         }
-        if (element instanceof SeaTunnelRow) {
-            sinkWriter.write((SeaTunnelRow) element);
-            sinkWriteCount.inc();
-            sinkWriteBytes.inc(((SeaTunnelRow) element).getBytesSize());
-            sinkWriterQPS.markEvent();
-        } else {
-            throw new InvalidClassException(
-                    "only support SeaTunnelRow at now, the element Class is " + element.getClass());
-        }
+        sinkWriter.write(element);
+        sinkWriteCount.inc();
+        sinkWriteBytes.inc(element.getBytesSize());
     }
 
     @Override
     public void flush(boolean endOfInput) throws IOException, InterruptedException {
-        // For Flink 1.20+, flush is called before prepareCommit
-        log.debug("Flushing sink writer, endOfInput: {}", endOfInput);
         if (endOfInput) {
-            log.info("End of input reached, preparing for final flush");
+            // Just mark that we've reached end of input, but don't close here
         }
     }
 
     @Override
-    public Collection<CommitWrapper<CommT>> prepareCommit()
-            throws IOException, InterruptedException {
-        log.debug("Preparing commit for checkpointId: {}", checkpointId);
-        Optional<CommT> commTOptional = sinkWriter.prepareCommit(checkpointId);
-        return commTOptional
-                .map(CommitWrapper::new)
-                .map(Collections::singletonList)
-                .orElse(Collections.emptyList());
-    }
-
-    @Override
-    public List<FlinkWriterState<WriterStateT>> snapshotState(long checkpointId)
-            throws IOException {
-        log.debug("Snapshotting state for checkpointId: {}", checkpointId);
-        List<FlinkWriterState<WriterStateT>> states =
-                sinkWriter.snapshotState(this.checkpointId).stream()
-                        .map(state -> new FlinkWriterState<>(this.checkpointId, state))
-                        .collect(Collectors.toList());
-        this.checkpointId++;
-        return states;
-    }
-
-    @Override
     public void close() throws Exception {
-        log.info("Closing FlinkSinkWriter for Flink 1.20+");
-        try {
-            sinkWriter.close();
-            context.getEventListener().onEvent(new WriterCloseEvent());
-        } finally {
-            try {
-                if (resourceManager != null) {
-                    resourceManager.close();
+        synchronized (this) {
+            if (!closed) {
+                try {
+                    sinkWriter.close();
+                } catch (Exception e) {
+                    System.err.println("Warning: Error closing sink writer: " + e.getMessage());
+                } finally {
+                    closed = true;
                 }
-            } catch (Throwable e) {
-                log.error("Error closing resourceManager", e);
             }
         }
     }
--- seatunnel-translation/seatunnel-translation-flink/seatunnel-translation-flink-20/src/main/java/org/apache/seatunnel/translation/flink/sink/FlinkSinkWriterContext.java
+++ seatunnel-translation/seatunnel-translation-flink/seatunnel-translation-flink-20/src/main/java/org/apache/seatunnel/translation/flink/sink/FlinkSinkWriterContext.java
@@ -25,7 +25,6 @@ import org.apache.seatunnel.translation.flink.metric.FlinkMetricContext;
 
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.connector.sink2.Sink;
-import org.apache.flink.api.connector.sink2.WriterInitContext;
 import org.apache.flink.metrics.MetricGroup;
 import org.apache.flink.streaming.api.operators.StreamingRuntimeContext;
 
@@ -37,36 +36,20 @@ import java.lang.reflect.Method;
 @Slf4j
 public class FlinkSinkWriterContext implements SinkWriter.Context {
 
-    private final Object initContext; // Can be either InitContext or WriterInitContext
+    private final Sink.InitContext initContext;
     private final int parallelism;
     private final EventListener eventListener;
-
-    public FlinkSinkWriterContext(WriterInitContext initContext, int parallelism) {
-        this.initContext = initContext;
-        this.parallelism = parallelism;
-        this.eventListener = new DefaultEventProcessor(getFlinkJobId(initContext));
-        log.info(
-                "FlinkSinkWriterContext initialized with WriterInitContext and parallelism: {} for Flink 1.20+",
-                parallelism);
-    }
 
     public FlinkSinkWriterContext(Sink.InitContext initContext, int parallelism) {
         this.initContext = initContext;
         this.parallelism = parallelism;
         this.eventListener = new DefaultEventProcessor(getFlinkJobId(initContext));
-        log.info(
-                "FlinkSinkWriterContext initialized with InitContext and parallelism: {} for Flink 1.20+",
-                parallelism);
+        log.info("FlinkSinkWriterContext initialized with parallelism: {}", parallelism);
     }
 
     @Override
     public int getIndexOfSubtask() {
-        if (initContext instanceof WriterInitContext) {
-            return ((WriterInitContext) initContext).getTaskInfo().getIndexOfThisSubtask();
-        } else if (initContext instanceof Sink.InitContext) {
-            return ((Sink.InitContext) initContext).getTaskInfo().getIndexOfThisSubtask();
-        }
-        throw new IllegalStateException("Unknown initContext type: " + initContext.getClass());
+        return initContext.getSubtaskId();
     }
 
     @Override
@@ -78,7 +61,7 @@ public class FlinkSinkWriterContext implements SinkWriter.Context {
     public MetricsContext getMetricsContext() {
         try {
             RuntimeContext runtimeContext = getRuntimeContext();
-            MetricGroup metricGroup = getMetricGroup();
+            MetricGroup metricGroup = initContext.metricGroup();
 
             if (runtimeContext != null && metricGroup != null) {
                 return new FlinkMetricContext(runtimeContext, metricGroup);
@@ -90,15 +73,6 @@ public class FlinkSinkWriterContext implements SinkWriter.Context {
             log.warn("Failed to create metrics context", e);
             return new FlinkMetricContext((MetricGroup) null);
         }
-    }
-
-    private MetricGroup getMetricGroup() {
-        if (initContext instanceof WriterInitContext) {
-            return ((WriterInitContext) initContext).metricGroup();
-        } else if (initContext instanceof Sink.InitContext) {
-            return ((Sink.InitContext) initContext).metricGroup();
-        }
-        return null;
     }
 
     @Override
@@ -224,15 +198,6 @@ public class FlinkSinkWriterContext implements SinkWriter.Context {
             log.debug("Failed to get RuntimeContext from fields", e);
         }
         return null;
-    }
-
-    private static String getFlinkJobId(WriterInitContext context) {
-        try {
-            return context.getJobInfo().getJobId().toString();
-        } catch (Exception e) {
-            log.warn("Get flink job id failed", e);
-            return null;
-        }
     }
 
     private static String getFlinkJobId(Sink.InitContext context) {
